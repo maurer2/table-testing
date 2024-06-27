474cef2d208bd519a23a3d723bf3b95864560fcd
Martin <mr.mmaurer@gmail.com>
2024-06-27 23:06:49 +0100

    Added headers attribute

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,16 @@ function App() {
 
   return (
     <main className="text-center my-4">
-      <table /* aria-label="table name" */ aria-describedby="table-title" className="border border-black m-auto text-left inline-grid gap-4 grid-cols-3">
+      <table
+        /* aria-label="table name" */ aria-describedby="table-title"
+        className="border border-black m-auto text-left inline-grid gap-4 grid-cols-3"
+      >
         <caption id="table-title" className="col-span-3">
           <h1>Table title</h1>
         </caption>
         <thead className="contents">
           <tr className="contents">
-            <th scope="col" className="">Header 1 for column</th>
+            <th scope="col">Header 1 for column</th>
             <th scope="col">Header 2 for column</th>
             <th scope="col" aria-sort="ascending">
               Header 3 for column
@@ -20,13 +23,22 @@ function App() {
         </thead>
         <tbody className="contents">
           <tr className="contents">
-            <th scope="rowgroup" className="col-span-3">Subheader for following rows and columns</th>
+            <th id="subheader-one" className="col-span-3">
+              Subheader for following rows and columns
+            </th>
           </tr>
           <tr className="contents">
-            <th scope="row">Subsubheader for columns in row</th>
-            <td>Column 2</td>
-            <td>Column 3</td>
+            <th scope="row" headers="subheader-one">Subsubheader for columns in row</th>
+            <td headers="subheader-one">Column 2</td>
+            <td headers="subheader-one">Column 3</td>
           </tr>
+          <tr className="contents">
+            <th scope="row" headers="subheader-one">Subsubheader for columns in row</th>
+            <td headers="subheader-one">Column 2</td>
+            <td headers="subheader-one">Column 3</td>
+          </tr>
+        </tbody>
+        <tbody className="contents">
           <tr className="contents">
             <th scope="row">Subsubheader for columns in row</th>
             <td>Column 2</td>
